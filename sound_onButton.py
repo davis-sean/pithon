fed2281df0c1670d041f4bfe1f18cbb38b9547c4
davis-sean <sadavis@davissystem.net>
2023-10-01 19:34:07 -0700

    Buffer

--- sound_onButton.py
+++ sound_onButton.py
@@ -14,6 +14,7 @@ parser.add_argument('path', metavar='Path', help='File Path')
 args = parser.parse_args()
 dirPath =  args.path
 
+pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 button = Button("GPIO17")
 dirArray = []
